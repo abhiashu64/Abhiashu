e4aa928e3e125fa3ebbf071e57e42198e8e20002
abhiashu64 <cookiesabhi0064@gmail.com>
2025-03-12 10:56:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,15 +10,15 @@ def hello_world():
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png">
+	<link rel="stylesheet" type="text/css" href="https://graph.org/file/2a5cb7bb0144ccfda3d37-60007d94009e55415f.jpg">
     <titleSudoR2spr Repository</title>
-	<link rel="icon" type="image/x-icon" href="https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png">
+	<link rel="icon" type="image/x-icon" href="https://graph.org/file/2a5cb7bb0144ccfda3d37-60007d94009e55415f.jpg">
 
 </head>
 
 <body>
     <div class="container" style="bg-dark text-red text-center py-3 mt-5">
-        <a href="https://github.com/nikhilsaini098" class="card">
+        <a href="https://github.com/abhiashu64" class="card">
             <p>
               />▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄<br 
 	      />██░▄▄▄░█░▄▄▀█▄░▄██░▀██░█▄░▄██<br 
@@ -33,9 +33,9 @@ def hello_world():
     </div>
 	<br></br><br></br><br></br>
 	<footer class="bg-dark text-white text-center py-3 mt-5">
-	<center><img loading="lazy" class="object-none object-center" src="https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png" width="240" height="120">
+	<center><img loading="lazy" class="object-none object-center" src="https://graph.org/file/2a5cb7bb0144ccfda3d37-60007d94009e55415f.jpg" width="240" height="120">
         Powered By SAINI 
-		<img loading="lazy" class="object-none object-center" src="https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png" width="240" height="120">
+		<img loading="lazy" class="object-none object-center" src="https://graph.org/file/2a5cb7bb0144ccfda3d37-60007d94009e55415f.jpg" width="240" height="120">
 		<div class="footer__copyright">
             <p class="footer__copyright-info">
                 © 2024 Video Downloader. All rights reserved.
